2cd17d4ff9d990f14680a24340ac0c5d371e9d8d
seongho <damadu01@gmail.com>
2024-03-08 17:39:44 +0900

    Expanded, Flexible & 상품 레이아웃 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(actions: [Icon(Icons.star)], title: Text('ddd'),),
-            body: SizedBox(
-              child: IconButton(
-                icon: Icon(Icons.star),
-                onPressed: (){},
-              )
+            appBar:AppBar(),
+            body: Container(
+              height: 150,
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Image.asset('camera.png', width: 150,),
+                  Container(
+                    width: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('카메라팝니다.', style: TextStyle(fontWeight: FontWeight.w700),),
+                        Text('금호동 3가'),
+                        Text('7000원'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite),
+                            Text('4'),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
         )
     );
