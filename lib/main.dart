7c8b9d93ff137cef2b1145dbd32f24a631924ac3
seongho <damadu01@gmail.com>
2024-03-11 08:36:47 +0900

    코드가 길어지면 커스텀 위젯만드세요

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
+// 변수 = 평생 안 바뀌는 것들은 변수에 받아도 상관없음. 아니면 성능 상 문제가 있을 수 있음.
+var a = SizedBox(
+  child: Text('안녕'),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,34 +17,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar:AppBar(),
-            body: Container(
-              height: 150,
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Image.asset('camera.png', width: 150,),
-                  SizedBox(
-                    width: 300,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('카메라팝니다.', style: TextStyle(fontWeight: FontWeight.w700),),
-                        Text('금호동 3가'),
-                        Text('7000원'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite),
-                            Text('4'),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
+            // 스크롤바 생김
+            // 스크롤 위침 감지
+            // 메모리 절약
+            body: ListView(
+              children: [
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕'),
+                Text('안녕')
+              ],
+            ),
         )
+    );
+  }
+}
+
+// 커스텀위젯 단점
+// state 관리 힘듦.
+
+// 재사용 많은 ui
+// 큰 위젯
+
+// extends: 복사 오른쪽 -> 왼쪽
+class ShopItem extends StatelessWidget {
+  // 어떤 파라미터를 넣을 수 있는지 정의
+  const ShopItem({super.key});
+  // 내가 만든 것들을 적용해주세요
+  @override
+  // 다트에서는 작명() 가 함수 정의 문법임.
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      child: Text('안녕'),
     );
   }
 }
