00d148d0bf81e77d9ca2ed32af0e5d357d412cbb
seongho <damadu01@gmail.com>
2024-03-11 09:02:45 +0900

    StatefulWidget 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,31 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   var a = 1;
+  var name = ['김영숙2', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            child: Text(a.toString()),
-            onPressed: (){
-              print(a);
-              a++;
-            },
-          ),
-            appBar:AppBar(),
+            appBar:AppBar( title: Text('App'),),
             body: ListView.builder(
                 itemCount: 3,
                 itemBuilder: (context, index){
-                  print(index);
-                  return Text(index.toString());
+                  return ListTile(
+                    title: Text(name[index]),
+                    trailing: TextButton(child: Text('123'),),
+                  );
                 })
         )
     );
