8f55aa678b3ab9bd7bf320c0229847c9f75868fd
seongho <damadu01@gmail.com>
2024-03-12 08:50:26 +0900

    Dialog/모달창 만드는 법과 context가 뭔지

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      home: MyApp()
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -17,20 +19,24 @@ class _MyAppState extends State<MyApp> {
   var like = [0, 0, 0];
 
   @override
-
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar:AppBar( title: Text('App'),),
-            body: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (context, index){
-                  return ListTile(
-                    title: Text(name[index]),
-                    trailing: TextButton(child: Text('123'),),
-                  );
-                })
-        )
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // context: 부모위젯의 정보를 담고있는 변수
+            showDialog(context: context, builder: (context) {
+              return Dialog(child: Text('안녕'));
+            });
+          },
+        ),
+        appBar: AppBar(title: Text('App'),),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(name[index]),
+              );
+            })
     );
   }
 }
