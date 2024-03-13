ab8b5fec06ceed7c2b9cd2dcacf5f6f23bcf5bd2
seongho <damadu01@gmail.com>
2024-03-13 11:24:39 +0900

    Dialog/모달창 만드는 법과 context가 뭔지 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   var like = [0, 0, 0];
   var name = ['김영숙', '홍길동', '피자집'];
 
-  up(index){
+  up(index) {
     setState(() {
       like[index]++;
     });
@@ -24,27 +24,60 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                      child: Container(
+                        height: 500,
+                        padding: EdgeInsets.all(30),
+                        child:
+                          Column(
+                            children: [
+                              TextField(),
+                              Row(
+                                children: [
+                                  TextButton(onPressed: () {},
+                                      child: Text('완료')),
+                                  TextButton(onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                      child: Text('취소'))
+                                ],
+                              )
+                            ],
+                          )
+                      )
+                  );
+                }
+            );
+          },
+          child: Text('버튼'),
+        ),
+        appBar: AppBar(),
+        body: ListView.builder(
           itemCount: like.length,
           itemBuilder: (context, index) {
             return Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(like[index].toString()),
-                      Text(name[index]),
-                    ],
-                  ),
-                  TextButton(onPressed: (){
-                    up(index);
-                  }, child: Text('좋아요')),
-                ],
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(like[index].toString()),
+                    Text(name[index]),
+                  ],
+                ),
+                TextButton(
+                    onPressed: () {
+                      up(index);
+                    },
+                    child: Text('좋아요')),
+              ],
             );
           },
-      )
-    );
+        ));
   }
 }
