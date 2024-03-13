42be6afa39d8be100949f33c5981cb5e75435ab4
seongho <damadu01@gmail.com>
2024-03-13 13:51:57 +0900

    자식 위젯이 부모 위젯의 state를 사용하고 싶으면 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var like = [0, 0, 0];
-  var name = ['김영숙', '홍길동', '피자집'];
+  var people = [
+    {'name': '김영숙', 'like': 0, 'phone': '010-2222-2222'},
+    {'name': '홍길동', 'like': 0, 'phone': '010-3333-3333'},
+    {'name': '피자집', 'like': 0, 'phone': '010-4444-4444'}
+  ];
+  var num = 0;
 
-  up(index) {
+  listAdd(n) {
     setState(() {
-      like[index]++;
+      people.add({'name': n, 'like': 0, 'phone': '010-xxxx-xxxx'});
+    });
+  }
+
+  numUp(index) {
+    setState(() {
+      // people[index]['like']를 int 타입으로 형변환하고, null일 경우 0을 기본값으로 사용합니다.
+      var currentLikes = people[index]['like'] as int? ?? 0;
+      people[index]['like'] = currentLikes + 1;
+    });
+  }
+
+  delete(index) {
+    setState(() {
+      people.removeAt(index);
     });
   }
 
@@ -29,36 +47,16 @@ class _MyAppState extends State<MyApp> {
             showDialog(
                 context: context,
                 builder: (context) {
-                  return Dialog(
-                      child: Container(
-                        height: 500,
-                        padding: EdgeInsets.all(30),
-                        child:
-                          Column(
-                            children: [
-                              TextField(),
-                              Row(
-                                children: [
-                                  TextButton(onPressed: () {},
-                                      child: Text('완료')),
-                                  TextButton(onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                      child: Text('취소'))
-                                ],
-                              )
-                            ],
-                          )
-                      )
-                  );
-                }
-            );
+                  return Dialog(child: Dialog2(listAdd: listAdd));
+                });
           },
           child: Text('버튼'),
         ),
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(num.toString()),
+        ),
         body: ListView.builder(
-          itemCount: like.length,
+          itemCount: people.length,
           itemBuilder: (context, index) {
             return Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -66,18 +64,60 @@ class _MyAppState extends State<MyApp> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(like[index].toString()),
-                    Text(name[index]),
+                    Text(people[index]['like'].toString()),
+                    Text(people[index]['name'].toString()),
+                    Text(people[index]['phone'].toString()),
                   ],
                 ),
                 TextButton(
                     onPressed: () {
-                      up(index);
+                      numUp(index);
                     },
                     child: Text('좋아요')),
+                TextButton(
+                    onPressed: () {
+                      delete(index);
+                    },
+                    child: Text('삭제')),
               ],
             );
           },
+        ));
+  }
+}
+
+class Dialog2 extends StatelessWidget {
+  Dialog2({super.key, this.listAdd});
+  final listAdd;
+
+  final text = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 500,
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            TextField(
+              controller: text,
+            ),
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      text.text.isEmpty ? null : listAdd(text.text);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('완료')),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('취소'))
+              ],
+            )
+          ],
         ));
   }
 }
