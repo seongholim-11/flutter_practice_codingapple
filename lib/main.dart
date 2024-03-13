868ab8d59094cc915856caeda3cd5430029ccccb
seongho <damadu01@gmail.com>
2024-03-13 10:04:45 +0900

    플러터 Typography와 Layout 만드는 법 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,43 @@ void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var a = 1;
-  var total = 3;
-  var name = ['김영숙2', '홍길동', '피자집'];
-  var like = [0, 0, 0];
-
-  addOne() {
-    setState(() {
-      total++;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // context: 부모위젯의 정보를 담고있는 변수
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return DialogUI(state: name, addOne: addOne);
-                });
-          },
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          width: 300,
+          height: 100,
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/camera.png',
+                width: 100,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('123'),
+                    Text('성동구 행당동'),
+                    Text('210,000'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end, // 아이콘과 텍스트 오른쪽 정렬
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        appBar: AppBar(
-          title: Text(total.toString()),
-        ),
-        body: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(name[index]),
-              );
-            }));
-  }
-}
-
-class DialogUI extends StatelessWidget {
-  DialogUI({Key? key, this.state, this.addOne}) : super(key: key);
-
-  final state;
-  final addOne;
-  var inputData = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(
-        child: Text('up'),
-        onPressed: () {
-          addOne();
-        },
       ),
     );
   }
